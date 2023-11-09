ad7ec8aebf2620b7cfe3db62c9fed8991a2818b0
chuifu Chen <2401829463@qq.com>
2023-11-09 19:58:13 +0800

    test

---
+++ BUAAChat/src/MyUtil/MyUtil.java
@@ -0,0 +1,14 @@
+package MyUtil;
+
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * @author 西西弗
+ * @Description:
+ * @date 2023/11/9 19:24
+ */
+public class MyUtil {
+    public static void main(String[] args) {
+        
+    }
+}
